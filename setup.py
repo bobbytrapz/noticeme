58a882359b354bdaa39350e0b2aac1ae6cc90c14
Otto Juno <ottojuno@gmail.com>
2018-12-29 05:18:30 -0500

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+from os import path
+cwd = path.abspath(path.dirname(__file__))
+with open(path.join(cwd, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name='noticeme',
       description=("Provides a framework for building watchers."
-                   "Includes a watcher utility program "
-                   "that allows you to create watchers declaratively."),
+                   " Includes a watcher utility program"
+                   " that allows you to create watchers declaratively."),
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='https://github.com/bobbytrapz/noticeme',
       author='Bobby',
       author_email='bobbytrapz@protonmail.com',
       license='MIT',
-      version='2018.12',
-      py_modules=['noticeme'],
-      python_requires='>=3.6',
+      version='2019.01',
+      packages=find_packages(),
+      python_requires='>=3.5',
       setup_requires=["cffi>=1.0.0"],
       install_requires=["cffi>=1.0.0"],
       cffi_modules=["noticeme/inotify_build.py:ffibuilder"],
@@ -18,8 +26,7 @@ setup(name='noticeme',
           'Development Status :: 4 - Beta',
           'License :: OSI Approved :: MIT License',
           'Operating System :: POSIX :: Linux',
-          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.5',
           'Topic :: Software Development :: Build Tools',
-          'Topic :: Software Development :: Python Modules',
           'Topic :: System :: Filesystems',
       ])
